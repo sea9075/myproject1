547acb0bde76df4283d8968178004926db40a07e
Kuei <sea9075@gmail.com>
2020-05-19 20:12:59 +0800

    Initial commit

---
+++ src/main/java/com/kuei/Person.java
@@ -0,0 +1,7 @@
+package com.kuei;
+
+public class Person {
+    public void hello(){
+        System.out.println("Holle world");;
+    }
+}
---
+++ src/main/kotlin/com/kuei/Hello.kt
@@ -0,0 +1,14 @@
+package com.kuei
+
+fun main(array: Array<String>){
+//    println("Hello kotlin")
+//      Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("Holle kotlin")
+    }
+}
